da317292ccf26cf77e27595dbf29ba04fec47ad8
roseHwang <beget12@naver.com>
2022-10-18 02:14:40 +0900

    [Level1] 탐욕법 체육복 알고리즘 풀이

---
+++ CodeTest/src/CodeTest19.java
@@ -0,0 +1,24 @@
+
+public class CodeTest19 {
+	public static void main(String[] args) {
+		int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+		
+	}
+	
+	
+	class Solution {
+	    public int solution(int n, int[] lost, int[] reserve) {
+	        /*
+	         [체육복] 요구사항 정리 
+	         1) 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복 빌려줄 수 있음
+	         2) 전체 학생 수 n 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 여벌의 체육복을
+	         가져온 학생들의 번호가 담긴 배열 reserve
+	         3) 체육수업 들을 수 있는 학생 최댓값 return 
+	         4) 전체 학생의 수는 2명 이상 30명 이하입니다.
+	         5) 체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+	        */
+	        int answer = 0;
+	        return answer;
+	    }
+	}
+}
